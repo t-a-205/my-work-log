8dc3b41cb7af7b33fafadc6f4c91a1d79b538548
t-a-205 <kosdaq2000@gmail.com>
2026-02-10 16:22:35 +0900

    진짜 배포 준비 끝

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>🚀 내 첫 업무일지 웹사이트!</h1>
+      <p>성공적으로 파일이 만들어졌어요.</p>
+    </div>
+  );
+}
+export default App;
